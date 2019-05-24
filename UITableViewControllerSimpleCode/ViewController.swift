65f57971fe880eca85fc35a90b98a9e821aa7f11
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-24 21:01:22 +0300

    done

--- UITableViewControllerSimpleCode/ViewController.swift
+++ UITableViewControllerSimpleCode/ViewController.swift
@@ -1,20 +1,41 @@
-//
-//  ViewController.swift
-//  UITableViewControllerSimpleCode
-//
-//  Created by Артем on 5/24/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let tableVC = UITableViewController(style: .plain)
+    var allItem = ["Vasya", "Dennis", "Artem", "Alex"]
+    let id = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createTableView()
+        self.view.addSubview(tableVC.tableView)
     }
-
-
+    
+    func createTableView() {
+        tableVC.tableView.register(UITableViewCell.self, forCellReuseIdentifier: id)
+        tableVC.tableView.delegate = self
+        tableVC.tableView.dataSource = self
+        tableVC.tableView.prefetchDataSource = self
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSourcePrefetching, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        print(indexPaths)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allItem.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
+        let item = allItem[indexPath.row]
+        cell.textLabel?.text = item
+        
+        return cell
+    }
+    
+}
