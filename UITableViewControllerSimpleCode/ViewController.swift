296bffe8db7b09d4f6bf30a7aeeb70eafe207bdf
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-24 20:44:33 +0300

    Initial Commit

---
+++ UITableViewControllerSimpleCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewControllerSimpleCode
+//
+//  Created by Артем on 5/24/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
